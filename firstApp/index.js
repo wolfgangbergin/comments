d34d1872b7873531dbe7185a180cdd4e8bc79a4d
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-10-29 08:34:55 -0400

    temp

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,20 +1,57 @@
-require('./wolfgang')
-const express = require('express')
-const app = express()
+require('./wolfgang');
+const express = require('express');
+const app = express();
 
+// app.use((req, res)=>{
+//   l(req)
+//     res.send('This is your response')
+// })
 
-app.use((req, res)=>{
-    l(req)
-    res.send('This is your response')
-})
+app.get('/', (req, res) => {
+  res.send('home page');
+});
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+  l(req.params);
+  const { subreddit, postId } = req.params;
+  res.send(
+    `<h1 style="color:red">subreddit:${subreddit},  postId: ${postId}</h1>`
+  );
+});
+
+app.post('/cats', (req, res) => {
+  res.send('this is a post request');
+});
+
+app.get('/cats', (req, res) => {
+  res.send('This is your response Meow');
+});
+
+app.get('/dogs', (req, res) => {
+  res.send('woof woof');
+});
+
+app.get('/search', (req, res) => {
+  l(req.query);
+  const {  wolfman, q } = req.query;
+  if (!q){
+    res.send(
+        `<h1 style="color:${q}">wolfman: ${'Nothing found if nothing searched'},  </h1>`
+      );
+  }
+  res.send(
+    `<h1 style="color:${q}">wolfman: ${wolfman},  </h1>`
+  );
+});
+
+app.get('*', (req, res) => {
+  res.send('Error does not compute');
+});
 
 app.listen(3000, () => {
-    l('listening on port 3000')
-    })
+  l('listening on port 3000');
+});
 
-    
 // globalThis.l = console.log;
-// globalThis.d = console.dir   
- // globalThis.wolfgang = {}
-
-
+// globalThis.d = console.dir
+// globalThis.wolfgang = {}
--- firstApp/kimbo.js
+++ firstApp/kimbo.js
@@ -3,15 +3,34 @@ const express = require('express')
 const app = express()
 
 
-app.use((req, res)=>{
-    l(req)
-    res.send('This is your response2')
+// app.use((req, res)=>{
+//     // l(req)
+//     res.send('<h1 style="color:red">This is your response2</h1>')
+// })
+
+app.get('/', (req, res)=>{
+    res.send('<h1 style="color:green">Homepage</h1>')
+})
+
+app.get('/B/:sub/:sub/', (req, res)=>{
+    res.send('<h1 style="color:red">sub /B</h1>')
 })
 
 
+app.get('/wolf', (req, res)=>{
+    res.send('<h1 style="color:red">wolf wolf</h1>')
+})
+
+app.get('/kimbo', (req, res)=>{
+    res.send('<h1 style="color:blue">kimbo kimbo </h1>')
+})
 
 app.listen(3001, () => {
-    l('listening on port 3000')
+    l('listening on port 3001')
+    })
+
+    app.get('*', (req, res)=>{
+        res.send('Error does not compute')
     })
 
 // globalThis.l = console.log;
