0b6757cd1f412f3d110d891cc81cbb056570a8a7
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-10-29 13:55:45 -0400

    moving .prettierrc

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,16 +1,16 @@
-require('./wolfgang');
-const express = require('express');
-const app = express();
-const path = require('path');
+require('./wolfgang')
+const express = require('express')
+const app = express()
+const path = require('path')
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-  res.render('home');
-});
+  res.render('home')
+})
 
 app.listen(3000, () => {
-  l('listening on port 3000');
+  l('listening on port 3000')
   l(__dirname)
-});
+})
--- firstApp/kimbo.js
+++ firstApp/kimbo.js
@@ -2,68 +2,55 @@ require('./wolfgang')
 const express = require('express')
 const app = express()
 
-
 // app.use((req, res)=>{
 //     // l(req)
 //     res.send('<h1 style="color:red">This is your response2</h1>')
 // })
 
-app.get('/', (req, res)=>{
-    res.send('<h1 style="color:green">Homepage</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1 style="color:green">Homepage</h1>')
 })
 
-app.get('/B/:sub/:sub/', (req, res)=>{
+app.get('/B/:sub/:sub/', (req, res) => {
   res.send('<h1 style="color:red">sub /B</h1>')
 })
 
-
-app.get('/wolf', (req, res)=>{
-    res.send('<h1 style="color:red">wolf wolf</h1>')
+app.get('/wolf', (req, res) => {
+  res.send('<h1 style="color:red">wolf wolf</h1>')
 })
 
-app.get('/kimbo', (req, res)=>{
-    res.send('<h1 style="color:blue">kimbo kimbo </h1>')
+app.get('/kimbo', (req, res) => {
+  res.send('<h1 style="color:blue">kimbo kimbo </h1>')
 })
 
 app.listen(3001, () => {
-    l('listening on port 3001')
-    })
+  l('listening on port 3001')
+})
 
-    app.get('/a/:tuesday/:thursday/search', (req, res) => {
-        l(req.query);
-        const {  wolfman, color } = req.query;
-        const {tuesday, thursday} = req.params
-        if (!color){
-          res.send(
-              `<h1 style="color:${color}">No color givin,  </h1>`
-            );
-        }
-        res.send(
-          `<h1 style="color:${color}">wolfman: ${wolfman}, tuesday: ${tuesday}, thursday: ${thursday}, </h1>`
-        );
-      });
+app.get('/a/:tuesday/:thursday/search', (req, res) => {
+  l(req.query)
+  const { wolfman, color } = req.query
+  const { tuesday, thursday } = req.params
+  if (!color) {
+    res.send(`<h1 style="color:${color}">No color givin,  </h1>`)
+  }
+  res.send(`<h1 style="color:${color}">wolfman: ${wolfman}, tuesday: ${tuesday}, thursday: ${thursday}, </h1>`)
+})
 
+app.post('/a/:tuesday/:thursday/search', (req, res) => {
+  l(req.query)
+  const { wolfman, color } = req.query
+  const { tuesday, thursday } = req.params
+  if (!color) {
+    res.send(`<h1 style="color:${color}">No color givin,  </h1>`)
+  }
+  res.send(`<h1 style="color:${color}">wolfman: ${wolfman}, tuesday: ${tuesday}, thursday: ${thursday}, </h1>`)
+})
 
-      app.post('/a/:tuesday/:thursday/search', (req, res) => {
-        l(req.query);
-        const {  wolfman, color } = req.query;
-        const {tuesday, thursday} = req.params
-        if (!color){
-          res.send(
-              `<h1 style="color:${color}">No color givin,  </h1>`
-            );
-        }
-        res.send(
-          `<h1 style="color:${color}">wolfman: ${wolfman}, tuesday: ${tuesday}, thursday: ${thursday}, </h1>`
-        );
-      });
-
-    app.get('*', (req, res)=>{
-        res.send('Error does not compute')
-    })
+app.get('*', (req, res) => {
+  res.send('Error does not compute')
+})
 
 // globalThis.l = console.log;
-// globalThis.d = console.dir   
- // globalThis.wolfgang = {}
-
-
+// globalThis.d = console.dir
+// globalThis.wolfgang = {}
