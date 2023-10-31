d0607ef8f3a42313b7432001142cf13c1e7aa059
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-10-31 09:32:40 -0400

    temp

--- firstApp/index.js
+++ firstApp/index.js
@@ -4,7 +4,7 @@ const app = express()
 const path = require('path')
 const redditData = require('./data.json')
 
-
+app.use(express.static(path.join(__dirname, './public')))
 
 const tuesday = 'tuesday'
 
